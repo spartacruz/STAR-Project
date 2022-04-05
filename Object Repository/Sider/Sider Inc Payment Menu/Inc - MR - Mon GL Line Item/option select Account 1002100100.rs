d42a918cd8198880e26d798a6a456788b1aa5cf7
Yuri <yuri.barru@gmail.com>
2022-04-05 17:57:46 +0700

    add SINC-12848/TCMSTAR-14428 and add new test object 1002100100

---
+++ Object Repository/Sider/Sider Inc Payment Menu/Inc - MR - Mon GL Line Item/option select Account 1002100100.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>option select Account 1002100100</name>
+   <tag></tag>
+   <elementGuidId>5e018579-01ba-4c36-99d6-b845e5b3c5ae</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>div[title*='1002100100']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>CSS</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
