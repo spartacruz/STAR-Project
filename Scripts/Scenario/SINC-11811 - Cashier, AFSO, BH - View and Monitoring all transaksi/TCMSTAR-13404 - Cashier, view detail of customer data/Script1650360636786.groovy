6cacb324c17e81ab134026b52b7330b5bf713bf8
Yuri <yuri.barru@gmail.com>
2022-04-19 17:15:21 +0700

    add SINC-11811/TCMSTAR-13404 (unfinished) and 'Verify Transaction Detail'

---
+++ Scripts/Scenario/SINC-11811 - Cashier, AFSO, BH - View and Monitoring all transaksi/TCMSTAR-13404 - Cashier, view detail of customer data/Script1650360636786.groovy
@@ -0,0 +1,36 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+import java.util.Random
+
+
+WebUI.callTestCase(findTestCase('Scenario/SINC-11811 - Cashier, AFSO, BH - View and Monitoring all transaksi/TCMSTAR-13355 - Cashier, go to submenu Monitoring'), 
+    [:], FailureHandling.STOP_ON_FAILURE)
+/*
+Random rand = new Random();
+int upperbound = 10;
+
+//generate random values from 0-10
+int int_random = rand.nextInt(upperbound)
+if (int_random == 0) {
+	int_random = 1
+}*/
+
+WebUI.callTestCase(findTestCase('Scenario/SINC-11811 - Cashier, AFSO, BH - View and Monitoring all transaksi/mod/Verify Transaction Detail'),
+	[('nrow') : '1'], FailureHandling.STOP_ON_FAILURE)
+
