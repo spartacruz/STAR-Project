2dbccc110edd882a88c5703d998e5ca6945a250d
Yuri <yuri.barru@gmail.com>
2022-04-21 14:55:33 +0700

    add new test suite - SINC-12845

---
+++ Test Suites/SINC-12845 - As a Cashier, View n Maintain GL Line Account.ts
@@ -0,0 +1,84 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>SINC-12845 - As a Cashier, View n Maintain GL Line Account</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>2c83f096-4660-450d-8228-041ca554d143</testSuiteGuid>
+   <testCaseLink>
+      <guid>9e7f0f31-cb23-4522-945f-a813eb6a7a2a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Scenario/SINC-12845 - As a Cashier, View n Maintain GL Line Account/TCMSTAR-14409 - As a Cashier, see List of GL Line Item based on defined filter</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>2ee55d8c-9233-4a5a-ac4c-007f95de8246</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Scenario/SINC-12845 - As a Cashier, View n Maintain GL Line Account/TCMSTAR-14411 - As a Cashier, see List of GL Line Item based on defined filter and check all table field</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c7343b9f-3be0-44a0-986c-7c9643021989</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Scenario/SINC-12845 - As a Cashier, View n Maintain GL Line Account/TCMSTAR-14412 - As a Cashier, see List of GL Line Item and back to previous menu (Monitoring GL Line Item)</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>a58ee0a4-23cf-4837-95e8-2ad6fb84c7c6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Scenario/SINC-12845 - As a Cashier, View n Maintain GL Line Account/TCMSTAR-14414 - Cashier, Use search text box find data on GL Account field</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>3f511981-7b34-419d-b5ff-859656834d6e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Scenario/SINC-12845 - As a Cashier, View n Maintain GL Line Account/TCMSTAR-14416 - Cashier, Use search text box find data on GL Description field</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>72959155-e2a9-4172-8a51-0e187845cd41</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Scenario/SINC-12845 - As a Cashier, View n Maintain GL Line Account/TCMSTAR-14418 - Cashier, Use search text box find data on Posting Number field</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>45227656-b4cf-4262-a93b-db7aa13c7dfb</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Scenario/SINC-12845 - As a Cashier, View n Maintain GL Line Account/TCMSTAR-14420 - Cashier, Use search text box find data on Doc. Reference field</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>450b645a-eb49-40e9-b8a2-13f8d46febfc</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Scenario/SINC-12845 - As a Cashier, View n Maintain GL Line Account/TCMSTAR-14422 - Cashier, Use search text box find unexist data</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ed546578-a8c6-4783-83eb-36b7d1eeee50</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Scenario/SINC-12845 - As a Cashier, View n Maintain GL Line Account/TCMSTAR-14425 - Cashier, See the page 3 of the result</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>24e2778b-f0b0-45f3-a2da-e147b827e46f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Scenario/SINC-12845 - As a Cashier, View n Maintain GL Line Account/TCMSTAR-14427 - Cashier, see the detail transaction of the GL Account</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
